4e28ae1e54eb5eb8cceb61b36a4946516ff694de
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-10-18 17:27:38 -0400

    chore: show options page on startup

--- src/background.ts
+++ src/background.ts
@@ -1,10 +1,9 @@
 // uncomment if you want options.html to be opened after extension is installed
-/*
+
 chrome.runtime.onInstalled.addListener(async ({ reason }) => {
-  if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
-    chrome.tabs.create({
-      url: 'options.html',
-    });
-  }
+	if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
+		chrome.tabs.create({
+			url: "options/index.html",
+		});
+	}
 });
-*/

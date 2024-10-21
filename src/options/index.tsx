ed9bb6fabb2f0ba7c1193cb5f6a666af5a6c50f0
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-10-21 13:08:42 -0400

    feat: add styling and tailwind theme

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -4,9 +4,19 @@ const container = document.getElementById("root");
 if (container) {
   const root = createRoot(container);
   root.render(
-    <div className="p-6">
-      <h1 className="text-2xl">Hello, World</h1>
-      <p className="text-base">This options page is rendered with React</p>
+    <div className="flex flex-col items-center">
+      <h1 className="text-primary">Hi!</h1>
+      <p>Welcome to your Hackatime extension!</p>
+
+      <p>
+        This is a work in progress. The goal is to help you track the time you
+        spend on different websites.
+      </p>
+
+      <form method="post" className="flex flex-row self-center w-[60%]">
+            <input type="text" name="token" className="input-default m-2" required placeholder="Enter your hackatime token here" />
+            <button type="submit" className="btn-primary m-2">Submit</button>
+      </form>
     </div>
   );
 }

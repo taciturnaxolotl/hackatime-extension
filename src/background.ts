75bd26a82aa6398506b9c6eaab33e2cfa9904443
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-10-23 15:39:16 -0400

    feat: add tab permissions and implement tab activation listener for icon updates

--- src/background.ts
+++ src/background.ts
@@ -1,9 +1,29 @@
-// uncomment if you want options.html to be opened after extension is installed
-
+// open the options page on install
 chrome.runtime.onInstalled.addListener(async ({ reason }) => {
 	if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
 		chrome.tabs.create({
 			url: "options/index.html",
 		});
 	}
+});
+
+// listen to the current tab
+chrome.tabs.onActivated.addListener((activeInfo) => {
+	chrome.tabs.get(activeInfo.tabId, (tab) => {
+		console.log({ status: tab.status, title: tab.title, url: tab.url });
+	});
+
+	chrome.storage.local.get("token", (result) => {
+		// if token.token doesn't exist then gray the icon
+		console.log(result);
+		if (!result.token) {
+			chrome.action.setIcon({
+				path: "icons/gray.png",
+			});
+		} else {
+			chrome.action.setIcon({
+				path: "icons/128.png",
+			});
+		}
+	});
 });

d7a941320800f966ded279de18ff3c4b137f83a5
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-10-24 11:38:49 -0400

    feat: implement tab activity tracking and heartbeat logging

--- src/background.ts
+++ src/background.ts
@@ -30,12 +30,6 @@ setInterval(() => {
 // listen to the current tab activation
 chrome.tabs.onActivated.addListener((activeInfo) => {
 	chrome.tabs.get(activeInfo.tabId, (tab) => {
-		console.log({
-			trigger: "activate",
-			status: tab.status,
-			title: tab.title,
-			url: tab.url,
-		});
 		handleTabUpdate(tab.url, cache);
 	});
 });
@@ -45,13 +39,88 @@ chrome.webNavigation.onCompleted.addListener((details) => {
 	chrome.tabs.get(details.tabId, (tab) => {
 		// ignore the newtab page
 		if (tab.url !== "chrome://newtab/") {
-			console.log({
-				trigger: "navigate",
-				status: tab.status,
-				title: tab.title,
-				url: tab.url,
-			});
 			handleTabUpdate(tab.url, cache);
 		}
 	});
 });
+
+let lastTab: { id: number; ts: Date } | null = null;
+const timePerTab = new Map<
+	number,
+	{ time: number; url: string; title: string }
+>();
+const inactiveTime = 120000; // 2 minutes inactivity threshold
+const heartbeatInterval = 30000; // 3 minutes interval
+
+// Listen for tab activation
+chrome.tabs.onActivated.addListener((activeInfo) => {
+	chrome.tabs.get(activeInfo.tabId, (tab) => {
+		if (lastTab) {
+			const timeSpent = Date.now() - lastTab.ts.getTime();
+			const lastTabFull = timePerTab.get(lastTab.id);
+			timePerTab.set(lastTab.id, {
+				time: (lastTabFull?.time || 0) + timeSpent,
+				url: lastTabFull?.url || "",
+				title: lastTabFull?.title || "",
+			});
+		}
+
+		if (!timePerTab.has(activeInfo.tabId)) {
+			timePerTab.set(activeInfo.tabId, {
+				time: 0,
+				url: tab.url || "",
+				title: tab.title || "",
+			});
+		}
+
+		lastTab = {
+			id: activeInfo.tabId,
+			ts: new Date(),
+		};
+
+		console.log("Tab activated", Array.from(timePerTab.entries()));
+	});
+});
+
+setInterval(() => {
+	if (!lastTab) return;
+	chrome.tabs.get(lastTab.id, (tab) => {
+		if (tab?.id && lastTab) {
+			const timeSpent = Date.now() - lastTab.ts.getTime();
+			const lastTabFull = timePerTab.get(lastTab.id);
+			timePerTab.set(lastTab.id, {
+				time: (lastTabFull?.time || 0) + timeSpent,
+				url: lastTabFull?.url || "",
+				title: lastTabFull?.title || "",
+			});
+
+			lastTab = {
+				id: tab.id,
+				ts: new Date(),
+			};
+
+			// get largest amount of time tab
+			const tabId = Array.from(timePerTab.keys()).reduce((a, b) =>
+				(timePerTab.get(a)?.time || 0) > (timePerTab.get(b)?.time || 0) ? a : b,
+			);
+
+			const hbTab = timePerTab.get(tabId);
+			console.log("Heartbeat", hbTab);
+
+			// Clear the time per tab after heartbeat
+			timePerTab.clear();
+
+			// set current tab as last tab and add it to the map
+			lastTab = {
+				id: tabId,
+				ts: new Date(),
+			};
+
+			timePerTab.set(tabId, {
+				time: 0,
+				url: tab.url || "",
+				title: tab.title || "",
+			});
+		}
+	});
+}, heartbeatInterval);
